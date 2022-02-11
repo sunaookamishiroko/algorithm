cb47603266420b899f0ec3d2a825579e3bdbf2a5
이서윤 <idfn3535@gmail.com>
2022-02-12 02:40:04 +0900

    Feat : 더 짧은 시간복잡도를 가질 수 있는 주석 추가

--- codingtestbook/Greedy/2. 숫자 카드 게임-2.py
+++ codingtestbook/Greedy/2. 숫자 카드 게임-2.py
@@ -1,8 +1,10 @@
 N, M = map(int, input().split())
 answerlist = []
-
+# answer = 0
 for i in range(N):
     cardlist = (list(map(int, input().split())))
     answerlist.append(min(cardlist))
+    # answer = max(answer, min(cardlist))
 
+# print(answer)
 print(max(answerlist))
