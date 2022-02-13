0f6856b9d97b66c7886ee33a138086ece6f6f923
이서윤 <idfn3535@gmail.com>
2022-02-13 14:55:24 +0900

    Feat : 1이 될 때 까지

---
+++ codingtestbook/Greedy/3. 1이 될 때까지.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+
+count = 0
+
+while True:
+    if n % k == 0:
+        n = n / k
+    else:
+        n -= 1
+    count += 1
+
+    if n == 1:
+        break
+
+print(count)
